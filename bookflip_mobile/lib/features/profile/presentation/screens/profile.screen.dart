879ddecebe14f871cfa03f58878b75c356075255
Fadel Akram <af8356207@gmail.com>
2025-12-03 23:37:18 +0100

    profile screen setup

---
+++ bookflip_mobile/lib/features/profile/presentation/screens/profile.screen.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
+
+
+
+class ProfileScreen extends ConsumerStatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends ConsumerState<ProfileScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Profile"),
+    );
+  }
+}
