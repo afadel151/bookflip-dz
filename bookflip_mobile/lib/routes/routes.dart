99b51df71060565cf22c609bccad36217df33953
Fadel Akram <af8356207@gmail.com>
2025-12-03 23:36:07 +0100

    updated router to use Shell Branching

--- bookflip_mobile/lib/routes/routes.dart
+++ bookflip_mobile/lib/routes/routes.dart
@@ -1,66 +1,118 @@
 import 'package:bookflip_mobile/features/auth/presentation/screens/login.screen.dart';
 import 'package:bookflip_mobile/features/auth/presentation/screens/register.screen.dart';
+import 'package:bookflip_mobile/features/chat/presentation/screens/chats_screen.dart';
 import 'package:bookflip_mobile/features/explore/presentation/screens/explore.screen.dart';
 import 'package:bookflip_mobile/features/guest/presentation/screens/intro.dart';
+import 'package:bookflip_mobile/features/library/presentation/screens/library_screen.dart';
+import 'package:bookflip_mobile/features/profile/presentation/screens/profile.screen.dart';
+import 'package:bookflip_mobile/features/settings/presentation/screens/settings_screen.dart';
 import 'package:bookflip_mobile/routes/go_router_refresh_stream.dart';
+import 'package:bookflip_mobile/utils/main_app_shell.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 
-
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 part 'routes.g.dart';
 
-
-enum AppRoutes { 
-  intro,
-  login,
-  register,
-  main  
-}
-
+enum AppRoutes { intro, login, register, main,profile,chats,library,settings }
 
 final firebaseProvider = Provider<FirebaseAuth>((ref) {
   return FirebaseAuth.instance;
 });
-
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
 @riverpod
 GoRouter goRouter(Ref ref) {
   final firebaseAuth = ref.watch(firebaseProvider);
   return GoRouter(
+    navigatorKey: _rootNavigatorKey,
     initialLocation: '/intro',
     debugLogDiagnostics: true,
-    redirect: (context, state)  {
+    redirect: (context, state) {
       final isLogdedIn = firebaseAuth.currentUser != null;
-      if (isLogdedIn && (state.uri.toString() == 'login' || state.uri.toString() == 'register' ) ) {
+      final path = state.fullPath;
+      if (isLogdedIn && (path == '/login' || path == '/register' || path == '/intro')) {
         return '/main';
-      }else if (!isLogdedIn && state.uri.toString().startsWith('/main')){
-        return 'login';
+      }else if (!isLogdedIn && 
+          (path!.startsWith('/main') || 
+           path.startsWith('/library') || 
+           path.startsWith('/profile') ||
+           path.startsWith('/chats') || 
+           path.startsWith('/settings'))) {
+        return '/login';
       }
       return null;
     },
     refreshListenable: GoRouterRefreshStream(firebaseAuth.authStateChanges()),
     routes: [
-      GoRoute(
-        path: '/main',
-         name: AppRoutes.main.name,
-         builder:(context, state) => ExploreScreen(),
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return MainAppShell(navigationShell: navigationShell);
+        },
+        branches: [
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/main',
+                name: AppRoutes.main.name,
+                builder: (context, state) => ExploreScreen(),
+              )
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/library',
+                name: AppRoutes.library.name,
+                builder: (context, state) => LibraryScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/profile',
+                name: AppRoutes.profile.name,
+                builder: (context, state) => ProfileScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/chats',
+                name: AppRoutes.chats.name,
+                builder: (context, state) => ChatsScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/settings',
+                name: AppRoutes.settings.name,
+                builder: (context, state) => SettingsScreen(),
+              ),
+            ],
+          ),
+        ],
       ),
       GoRoute(
         path: '/intro',
-         name: AppRoutes.intro.name,
-         builder:(context, state) => IntroScreen(),
+        name: AppRoutes.intro.name,
+        builder: (context, state) => IntroScreen(),
       ),
       GoRoute(
         path: '/login',
         name: AppRoutes.login.name,
-        builder:(context, state) => LoginScreen(),
+        builder: (context, state) => LoginScreen(),
       ),
       GoRoute(
         path: '/register',
         name: AppRoutes.register.name,
-        builder:(context, state) => RegisterScreen(),
+        builder: (context, state) => RegisterScreen(),
       ),
     ],
   );
